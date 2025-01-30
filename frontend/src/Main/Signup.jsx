03883632bfdf98dab16d57cbce1921dc4a259ced
abhirup7477 <abhirup7477@gmail.com>
2025-01-31 01:42:23 +0530

    Create Signup.jsx

---
+++ frontend/src/Main/Signup.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export default function Signup(){
+    return(
+        <>
+            <div className="signup-body">
+                <div className="signup-form-container">
+                    <form action="" className="signup-form">
+                        
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
